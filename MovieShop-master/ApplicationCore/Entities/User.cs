2f2950ad67ac2c91e406bd58761adfbf1183dedb
shuoheren <47304324+shuoheren@users.noreply.github.com>
2021-10-21 22:30:29 -0400

    Add files via upload

---
+++ MovieShop-master/ApplicationCore/Entities/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [MaxLength(128)]
+        public string? FirstName { get; set; }
+        [MaxLength(128)]
+        public string? LastName { get; set; }
+        [MaxLength(7)]
+        public DateTime? DateOfBirth { get; set; }
+        [MaxLength(256)]
+        public string? Email { get; set; }
+        [MaxLength(1024)]
+        public string? HashedPassword { get; set; }
+        [MaxLength(1024)]
+        public string? Salt { get; set; }
+        [MaxLength(16)]
+        public string? PhoneNumber { get; set; }
+        public bool? TwoFactorEnabled { get; set; }
+        [MaxLength(7)]
+        public DateTime? LockoutEndDate { get; set; }
+        [MaxLength(7)]
+        public DateTime? LastLoginDateTime { get; set; }
+        public bool? isLocked { get; set; }
+        public int? AccessFailedCount { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ MovieShop-master/ApplicationCore/RepositoryInterfaces/IMovieRepository.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.RepositoryInterfaces
+{
+   public interface IMovieRepository
+    {
+        // method thtas gonn aget 30 highest revenue movies
+        IEnumerable<Movie> GetTop30RevenueMovies();
+    }
+}
