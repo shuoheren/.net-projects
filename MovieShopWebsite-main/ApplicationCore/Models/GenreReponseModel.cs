cde7e556a82cc6faefd530826d4ad71bcbbbf258
shuoheren <47304324+shuoheren@users.noreply.github.com>
2021-10-31 23:00:28 -0400

    Delete MovieShopWebsite-main directory

--- MovieShopWebsite-main/ApplicationCore/Models/GenreReponseModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ApplicationCore.Models
-{
-    public class GenreResponseModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- MovieShopWebsite-main/ApplicationCore/Models/UserLoginRequestModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ApplicationCore.Models
-{
-    public class UserLoginRequestModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
